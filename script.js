9b0ebf3c11e5a1b6d8fd1aa34a7576c029587be4
khalad007 <www.arifhossain49919@gmail.com>
2023-09-01 06:23:07 -0700

    navbar added

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('yes i am ')
+const videoDB = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/videos/category/1000');
+    const data = await res.json();
+    const videos1 = data.data;
+    displayVideo(videos1);
+    
+
+}
+
+const displayVideo = videos1 => {
+    videos1.forEach(videos2 =>{
+        console.log(videos2);
+    })
+}
+
+videoDB();
