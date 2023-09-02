8d465e62400cbace13682f649aaedfe649891a29
khalad007 <www.arifhossain49919@gmail.com>
2023-09-02 00:58:25 -0700

    category button added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// fetching card data 
 const videoDB = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/videos/category/1000');
     const data = await res.json();
@@ -7,6 +8,47 @@ const videoDB = async () => {
 
 
 }
+
+// fetching all catagory data 
+// fetching all catagory data 
+// fetching all catagory data 
+
+const catagoryDB = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/videos/categories');
+    const data = await res.json();
+    const catagory1 = data.data;
+    displayCatagory(catagory1);
+    // console.log(catagory1);
+
+
+}
+catagoryDB();
+// all catagory section 
+const displayCatagory = catagory1 => {
+    // console.log(catagory1);
+
+    const catagoryContainer = document.getElementById('catagory-container');
+    catagory1.forEach(catagory2 => {
+        console.log(catagory2);
+
+        // create a div 
+        const catagoryCard = document.createElement('div');
+        catagoryCard.classList = `items-center text-center mt-5 mb-8`;
+
+        // set innerhtml
+        catagoryCard.innerHTML = `
+        <button class="h-10 rounded-lg w-36 bg-gray-300">${catagory2.category}</button>
+        `;
+
+        // appendChild 
+
+        catagoryContainer.appendChild(catagoryCard);
+    })
+
+}
+
+
+// card body section 
 
 const displayVideo = videos1 => {
     // console.log(videos1);
@@ -28,10 +70,10 @@ const displayVideo = videos1 => {
                 <p>${videos2.title}</p>
                 
             </div>
-            <div class="ml-[75px] grid grid-cols-2">
-                <h4>${videos2.authors[0].profile_name}</h4><span>Thik img</span>
+            <div class="ml-[75px] grid grid-cols-2 justify-center text-xs">
+                <h4>${videos2.authors[0].profile_name}</h4><span>${(videos2.authors[0].verified === false) ? "" : `<img src="photos/fi_10629607.svg"`}</span>
             </div>
-            <h5 class="ml-[75px]">${videos2.others.views} views</h5>
+            <h5 class="ml-[75px] text-xs mb-3 pt-3">${videos2.others.views} views</h5>
         `;
 
         videoContainer.appendChild(videoCard);
@@ -42,27 +84,3 @@ const displayVideo = videos1 => {
 
 videoDB();
 
-
-// const videoContainer = document.getElementById('video-container');
-// videos1.forEach(videos2 => {
-//     console.log(videos2);
-
-
-//     const videoCard = document.createElement('div');
-//     videoCard.classList = `card w-96 bg-gray-400 shadow-xl`;
-//     videoCard.inneHTML = `
-//     <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-//         <div class="card-body flex-row">
-//             <img class="w-9" src="photos/Ellipse 1.svg" alt="">
-//             <p>If a dog chews shoes whose shoes does he choose?</p>
-            
-//         </div>
-//         <div class="ml-[75px] flex-row">
-//             <h4>Name</h4><span>Thik img</span>
-//         </div>
-//         <h5 class="ml-[75px]">View</h5>
-//         `;
-
-
-//         videoContainer.appendChild(videoCard);
-// })
