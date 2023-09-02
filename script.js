f0748dd7d7c9ef7a45c84800d130463ae2315b7f
khalad007 <www.arifhossain49919@gmail.com>
2023-09-02 04:59:45 -0700

    category button done

--- script.js
+++ script.js
@@ -23,7 +23,9 @@ const catagoryDB = async () => {
 
 }
 catagoryDB();
-// all catagory section 
+
+//.................................... all catagory bUTTON section 
+
 const displayCatagory = catagory1 => {
     // console.log(catagory1);
 
@@ -37,7 +39,7 @@ const displayCatagory = catagory1 => {
 
         // set innerhtml
         catagoryCard.innerHTML = `
-        <button class="h-10 rounded-lg w-36 bg-gray-300">${catagory2.category}</button>
+        <button onclick="handleLoadCategories('${catagory2.category_id}')" class="h-10 rounded-lg w-36 bg-gray-300">${catagory2.category}</button>
         `;
 
         // appendChild 
@@ -45,42 +47,94 @@ const displayCatagory = catagory1 => {
         catagoryContainer.appendChild(catagoryCard);
     })
 
-}
+};
+const handleLoadCategories = async (catagory2) => {
 
+    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${catagory2}`);
+    const data = await res.json();
 
-// card body section 
-
-const displayVideo = videos1 => {
-    // console.log(videos1);
-
+    console.log(data.data);
     const videoContainer = document.getElementById('video-container');
+    videoContainer.innerHTML = "";
 
-    videos1.forEach(videos2 => {
-        console.log(videos2);
+    data.data.forEach((details) => {
+        console.log(details);
+        const div = document.createElement('div');
+        div.classList = `mb-10 ml-6 card w-72 bg-gray-100 shadow-xl`;
+          // 3 inner html set
+          div.innerHTML = `
+          <figure><img  class="h-52" src="${details.thumbnail}" alt="Shoes" /></figure>
+              <div class="card-body flex-row items-center">
+              <img class="w-9 h-9 rounded-full" src=${details.authors[0].profile_picture} alt="">
+                  <p>${details.title}</p>
+                  
+              </div>
+              <div class="ml-[75px] grid grid-cols-2 justify-center text-xs">
+                  <h4>${details.authors[0].profile_name}</h4><span>${(details.authors[0].verified === false) ? "" : `<img src="photos/fi_10629607.svg"`}</span>
+              </div>
+              <h5 class="ml-[75px] text-xs mb-3 pt-3">${details.others.views} views</h5>
+          `;
+          videoContainer.appendChild(div);
 
-        //2 create a div
-        const videoCard = document.createElement('div');
-        videoCard.classList = `mb-10 ml-6 card w-72 bg-gray-100 shadow-xl`;
-
-        // 3 inner html set
-        videoCard.innerHTML = `
-        <figure><img  class="h-52" src="${videos2.thumbnail}" alt="Shoes" /></figure>
-            <div class="card-body flex-row items-center">
-                <img class="w-9" src="photos/Ellipse 1.svg" alt="">
-                <p>${videos2.title}</p>
-                
-            </div>
-            <div class="ml-[75px] grid grid-cols-2 justify-center text-xs">
-                <h4>${videos2.authors[0].profile_name}</h4><span>${(videos2.authors[0].verified === false) ? "" : `<img src="photos/fi_10629607.svg"`}</span>
-            </div>
-            <h5 class="ml-[75px] text-xs mb-3 pt-3">${videos2.others.views} views</h5>
-        `;
-
-        videoContainer.appendChild(videoCard);
-        
-    });
-
+    })
+    
 }
 
-videoDB();
 
+handleLoadCategories(1000);
+
+
+// card body section .....////.......//.........////.........///............./////...........//............//////
+
+// const displayVideo = videos1 => {
+//     // console.log(videos1);
+
+//     const videoContainer = document.getElementById('video-container');
+
+//     videos1.forEach(videos2 => {
+//         console.log(videos2);
+
+//         //2 create a div
+//         const videoCard = document.createElement('div');
+//         videoCard.classList = `mb-10 ml-6 card w-72 bg-gray-100 shadow-xl`;
+
+//         // 3 inner html set
+//         videoCard.innerHTML = `
+//         <figure><img  class="h-52" src="${videos2.thumbnail}" alt="Shoes" /></figure>
+//             <div class="card-body flex-row items-center">
+//                 <img class="w-9 h-9 rounded-full" src=${videos2.authors[0].profile_picture} alt="">
+//                 <p>${videos2.title}</p>
+                
+//             </div>
+//             <div class="ml-[75px] grid grid-cols-2 justify-center text-xs">
+//                 <h4>${videos2.authors[0].profile_name}</h4><span>${(videos2.authors[0].verified === false) ? "" : `<img src="photos/fi_10629607.svg"`}</span>
+//             </div>
+//             <h5 class="ml-[75px] text-xs mb-3 pt-3">${videos2.others.views} views</h5>
+//         `;
+
+//         videoContainer.appendChild(videoCard);
+
+//     });
+
+// }
+
+// videoDB();
+
+// sort-by-view ..................sort-by-view .....................sort-by-view.............. sort-by-view ......sort-by-view ...
+
+// const sortByView = document.getElementById('sort-by-view');
+
+// document.getElementById('sort-by-view').addEventListener('click',function sortByView(){
+//     document.body.style.backgroundColor = 'pink';
+
+//    })
+
+// const sortByView = sortByView => {
+//     // console.log(sortByView);
+//     document.getElementById('sort-by-view').addEventListener('click', function () {
+//         document.body.style.backgroundColor = 'pink';
+//         // console.log(sortByView);
+
+//     });
+
+// }
